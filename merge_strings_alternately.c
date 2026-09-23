8a15844ab9e7a81e7533a783cd6d4956d568c440
Anna Chen <annaduen@gmail.com>
2026-09-24 02:03:55 +0800

    Simplify merging of two strings alternately
    Refactor string merging logic to simplify character addition.

--- merge_strings_alternately.c
+++ merge_strings_alternately.c
@@ -3,10 +3,8 @@ char * mergeAlternately(char * word1, char * word2){
     int len1 = strlen(word1), len2 = strlen(word2);
     char *word3 = (char *)malloc(len1 + len2 + 1);
     while ((i < len1) || (j < len2)){
-        if (i == len1) word3[k++] = word2[j++];
-        else if (j == len2) word3[k++] = word1[i++];
-        else if (i == j)    word3[k++] = word1[i++];
-        else    word3[k++] = word2[j++];
+        if (i < len1)    word3[k++] = word1[i++];
+        if (j < len2)    word3[k++] = word2[j++];
     }
     word3[k] = '\0';
     
