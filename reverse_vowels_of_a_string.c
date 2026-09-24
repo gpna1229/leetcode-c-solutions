3e7e26b5fd5cf5555049b95769afcc480f5f5dda
Anna Chen <annaduen@gmail.com>
2026-09-24 14:49:59 +0800

    Implement function to reverse vowels in a string

---
+++ reverse_vowels_of_a_string.c
@@ -0,0 +1,16 @@
+char* reverseVowels(char* s) {
+    int left = 0, right = strlen(s) - 1;
+    char vowels[] = "aeiouAEIOU";
+    while (left < right) {
+        while (left < right && strchr(vowels, s[left]) == NULL) left++;
+        while (left < right && strchr(vowels, s[right]) == NULL) right--;
+        if (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+    return s;
+}
